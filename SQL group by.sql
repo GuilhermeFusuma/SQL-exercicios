ba504e8e41e00dabff57c43f8119c8ff94dd2fdf
Guilherme Kenji Fusuma <165702644+GuilhermeFusuma@users.noreply.github.com>
2024-10-10 15:48:35 -0300

    update GROUP BY

--- SQL group by.sql
+++ SQL group by.sql
@@ -3,7 +3,7 @@ SELECT
 	ChannelKey,
 	SUM(SalesQuantity) AS 'Quantidade Vendida'
 FROM FactSales
-GROUP BY ChannelKey
+GROUP BY ChannelKey --Correto
 --Questão 1-b
 SELECT
 	StoreKey,
@@ -11,14 +11,14 @@ SELECT
 	SUM(ReturnQuantity) AS 'Quantidade Total Devolvida'
 FROM FactSales
 GROUP BY StoreKey
-ORDER BY StoreKey
+ORDER BY StoreKey --Correto
 --Questão 1-c
 SELECT
 	ChannelKey,
-	Sum(SalesAmount) AS 'Valor Total'
+	Sum(SalesQuantity) AS 'Valor Total'
 FROM FactSales
-WHERE DateKey LIKE '%2007%'
-GROUP BY ChannelKey
+WHERE DateKey BETWEEN '2007/01/01' AND '2007/12/31'
+GROUP BY ChannelKey --Data no formato errado e foi usado SalesAmount
 
 
 --Questão 2-A
@@ -28,7 +28,7 @@ SELECT
 FROM FactSales
 GROUP BY Productkey
 HAVING SUM(SalesAmount) > 5000000
-ORDER BY SUM(SalesAmount) DESC
+ORDER BY SUM(SalesAmount) DESC --Correto
 
 --Questão 2-b
 SELECT TOP(10)
@@ -42,42 +42,42 @@ ORDER BY SUM(SalesAmount) DESC
 --Questão 3-a
 SELECT TOP(1)
 	CustomerKey,
-	COUNT(SalesQuantity) AS 'Qtd Vendida'
+	SUM(SalesQuantity) AS 'Qtd Vendida'
 FROM FactOnlinesales
 GROUP BY CustomerKey
-ORDER BY COUNT(SalesQuantity) DESC
+ORDER BY SUM(SalesQuantity) DESC --Usado COUNT e não SUM
 
 
 --Questão 3-b
 SELECT TOP(3) 
-	ProductKey,
-	Count(SalesQuantity) AS 'Qtd Vendida'
+	ProductKey AS 'ID do Produto',
+	SUM(SalesQuantity) AS 'total Vendida'
 FROM FactOnlineSales
 WHERE CustomerKey = 19037
 GROUP BY Productkey
-ORDER BY Count(SalesQuantity) DESC
+ORDER BY SUM(SalesQuantity) DESC
 
 
 --Questão 4-a
 SELECT
 	BrandName,
-	COUNT(BrandName) AS 'Qtd de produtos'
+	COUNT(*) AS 'Qtd de produtos'
 FROM DimProduct
-GROUP BY BrandName
+GROUP BY BrandName --COUNT(BrandName)***
 
 --Questão 4-b
 SELECT 
 	ClassName,
 	AVG(UnitPrice) AS 'Média de Preços'
 FROM DimProduct
-GROUP BY ClassName
+GROUP BY ClassName --Correto
 
 --Questão 4-c
 SELECT
 	ColorName,
 	SUM(Weight) AS 'Peso Total'
 FROM DimProduct
-GROUP BY ColorName
+GROUP BY ColorName --Correto
 
 
 --Questão 5
@@ -87,7 +87,7 @@ SELECT
 FROM DimProduct
 WHERE BrandName = 'Contoso'
 GROUP BY StockTypeName
-ORDER BY Peso_Total DESC
+ORDER BY Peso_Total DESC --Correto
 
 
 --Questão 6
@@ -95,22 +95,18 @@ SELECT
 	BrandName,
 	COUNT(DISTINCT ColorId) AS Qtd_Cores
 FROM DimProduct
-GROUP BY BrandName
+GROUP BY BrandName --Correto
+--Nenhuma marca possui 16 cores
 
 
 --Questão 7
 SELECT 
 	Gender,
-	COUNT(CustomerKey) AS Qtd
-FROM DimCustomer
-WHERE Gender IS NOT NULL
-GROUP BY Gender
-SELECT
-	Gender,
+	COUNT(Gender) AS Qtd,
 	AVG(YearlyIncome) AS Média_Salarial
 FROM DimCustomer
 WHERE Gender IS NOT NULL
-GROUP BY Gender
+GROUP BY Gender --Correto
 
 
 --Questão 8
@@ -120,7 +116,7 @@ SELECT
 	AVG(YearlyIncome) AS Média_Salarial
 FROM DimCustomer
 WHERE Education IS NOT NULL
-GROUP BY Education
+GROUP BY Education --Correto
 
 
 --Questão 9
@@ -132,10 +128,11 @@ WHERE EndDate IS NULL
 GROUP BY DepartmentName
 
 
+
 --Questão 10
 SELECT
 	Title,
-	SUM(VacationHours)
+	SUM(VacationHours) AS Tempo_de_Férias
 FROM DimEmployee
-WHERE EndDate IS NULL AND Gender = 'F' AND	DepartmentName IN ('Production', 'Marketing', 'Finance')
+WHERE EndDate IS NULL AND Gender = 'F' AND	DepartmentName IN ('Production', 'Marketing', 'Engineering', 'Finance') AND HireDate BETWEEN '1999/01/01' AND '2000/12/31'
 GROUP BY Title
